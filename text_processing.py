c368031508f9b88931090f573831c0a80bc7e39e
wqying <qywong@uci.edu>
2026-05-13 00:10:10 -0700

    initial commit

---
+++ text_processing.py
@@ -0,0 +1,11 @@
+# import nltk
+# nltk.download('punkt')
+# from nltk.tokenize import word_tokenize
+
+
+# text = "Building an inverted index requires careful planning."
+# tokens = word_tokenize(text)
+# print(tokens) # test
+
+
+
