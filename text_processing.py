0cc56682d95409efdf9c4f1a4cbdd10178124a37
wqying <qywong@uci.edu>
2026-05-13 01:34:32 -0700

    added notes.txt
    Initial design and plan for the inverse indexer

--- text_processing.py
+++ text_processing.py
@@ -1,11 +1,66 @@
-# import nltk
-# nltk.download('punkt')
-# from nltk.tokenize import word_tokenize
+"""
+Most of the code here is from my submission for Assignment 1, tokenization
+"""
+
+import sys
+from nltk.stem import PorterStemmer
 
 
-# text = "Building an inverted index requires careful planning."
-# tokens = word_tokenize(text)
-# print(tokens) # test
+stemmer = PorterStemmer()
+
+def stem_tokens(tokens):
+    for token in tokens:
+        yield stemmer.stem(token)
+
+
+def tokenize_text(text):
+    """
+    Reads in text string and returns a list of the tokens in that text.
+    """
+    current_token = ""
+    for char in text:
+        if char.isascii() and char.isalnum():
+            current_token += char.lower()
+        else:
+            if current_token != "":
+                yield current_token
+                current_token = ""
+
+    if current_token != "":
+        yield current_token
+
+
+# Runtime is O(n), where n is the number of characters in the text
+# tokenize_text processes each character once with O(n), and appending each token to the list is O(1) per token, so this still remains linear in the size of the input
+def tokenize(text):
+    tokens = []
+    for token in tokenize_text(text):
+        tokens.append(token)
+
+    return tokens
+    
+
+def compute_word_frequencies(tokens):
+    """
+    Counts the number of occurrences of each token in the token list.
+    """
+    frequencies = {}
+    for token in tokens:
+        if token in frequencies:
+            frequencies[token] += 1
+        else:
+            frequencies[token] = 1
+
+    return frequencies
+
+
+def print_frequencies(frequencies):
+    """
+    Prints out the word frequency count by decreasing frequency.
+    """
+    sorted_items = sorted(frequencies.items(), key=lambda item: (-item[1], item[0]))
+    for token, count in sorted_items:
+        print(f"{token}\t{count}")
 
 
 
